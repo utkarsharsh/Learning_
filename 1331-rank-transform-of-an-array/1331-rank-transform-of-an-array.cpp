97ee53c3791718f88096f04db3f8ad8295a3b891
Utkarsh Upadhyay <116327759+utkarsharsh@users.noreply.github.com>
2026-07-12 13:02:58 +0530

    Time: 301 ms (5.25%), Space: 45.7 MB (18.27%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 0) return {};
-        vector<int> res(n);
-        vector<pair<int,int>> temp(n);
-        for (int i = 0; i < n; i++) temp[i] = {arr[i],i};
-        sort(temp.begin(),temp.end(),[](pair<int,int> &a , pair<int,int> &b) {
-            return a.first < b.first;
-        });
-        res[temp[0].second] = 1;
-        for (int i = 1; i < n; i++) {
-            if (temp[i].first == temp[i - 1].first) res[temp[i].second] = res[temp[i - 1].second];
-            else res[temp[i].second] = res[temp[i - 1].second] + 1;
+        map<int,int>m;
+        
+         vector<int>b=arr;
+         sort(b.begin(),b.end());
+        int rank=1;
+        for(int i=0;i<b.size();i++){
+         if(m.find(b[i])==m.end()){
+            m[b[i]]=rank;
+            cout<<b[i]<<" ";
+           rank++;
+         }
+           
         }
-        return res;
+            vector<int>ans;
+         for(int i=0;i<arr.size();i++){
+        ans.push_back(m[arr[i]]);
+        }
+        return ans;
+
     }
 };
